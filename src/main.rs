1e0ae31ba35021fc988c9c68db67c9a580aa30a7
nidumolu <snidumolu@gmail.com>
2025-10-12 19:09:08 +0530

    First checkin

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+
+
+fn fib (n: i32) -> i32 {
+    if n <= 0 {
+          return 0;
+    } else if n== 1{
+          return 1;
+} else {
+    return fib (n-1)  + fib(n-2);
+ }
+}
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+    for int in 0..15 {
+         println! ( "fibonacci ({}) => {}", int, fib(int));
+     }
+}
